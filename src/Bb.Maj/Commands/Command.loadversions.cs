e072f242ec048671d8bf0b815998e7634716c18f
gaelgael5 <gaelgael5@gmail.com>
2021-06-28 19:49:58 +0200

    update

--- src/Bb.Maj/Commands/Command.loadversions.cs
+++ src/Bb.Maj/Commands/Command.loadversions.cs
@@ -96,102 +96,26 @@ namespace Bb.Maj.Commands
                 var processToWaitOpt = validator.Option("--p", "specifiy a process to waiting to end");
                 var versionOpt = validator.Option("--v", "specifiy the version to install");
 
-
                 config.OnExecute(() =>
                 {
 
-                    System.Diagnostics.Process process = null;
+                    int processId = 0;
                     if (processToWaitOpt.HasValue())
-                        process = System.Diagnostics.Process.GetProcessById(int.Parse(processToWaitOpt.Value()));
+                        processId = int.Parse(processToWaitOpt.Value());
 
                     string name = null;
                     name = packageNameArgument.Value.TrimPath();
 
-                    Output.WriteLineStandard($"Explore http://github/{name}");
-                    var versions = name.GetUrls()
-                        .Where(c => c.Name == artefactNameArgument.Value.TrimPath())
-                        .OrderByDescending(c => c.Version)
-                        .ToList();
-
-                    ArtefactItem artefact;
+                    string version = null;
                     if (versionOpt.HasValue())
-                    {
-                        string version = versionOpt.Value().TrimPath();
-                        artefact = versions.FirstOrDefault(c => c.Version.ToString() == version);
-                    }
-                    else
-                        artefact = versions.FirstOrDefault();
+                        version = versionOpt.Value().TrimPath();
 
-                    if (artefact.Version.ToString() == CurrentVersionArgument.Value)
-                    {
-                        Output.WriteLineStandard($"the current version is uptdate");
-                        return 0;
-                    }
+                    GitHubVersionHelper.WriteLineStandard = (a) => Output.WriteLineStandard(a);
+                    GitHubVersionHelper.WriteLineError = (a) => Output.WriteLineError(a);
 
-                    var file = new FileInfo(Path.Combine(Path.GetTempPath(), artefact.Name));
-                    if (!file.Directory.Exists)
-                        file.Directory.Create();
+                    var _result = GitHubVersionHelper.RunUpdate(processId, name, version, artefactNameArgument.Value.TrimPath(), CurrentVersionArgument.Value, targetDirArgument.Value.TrimPath());
 
-                    Output.WriteLineStandard($"Download '{artefact.UrlDownload}'");
-                    bool success = artefact.UrlDownload.DownloadFile(file);
-
-                    int result;
-
-                    if (success)
-                    {
-
-                        var dirToExtract = new DirectoryInfo(Path.GetTempFileName());
-                        dirToExtract = new DirectoryInfo(Path.Combine(dirToExtract.Parent.FullName, dirToExtract.Name.Replace(".", "")));
-
-                        if (!dirToExtract.Exists)
-                            dirToExtract.Create();
-
-                        Output.WriteLineStandard($"extracting '{file.FullName}'");
-                        if (file.Extract(dirToExtract))
-                        {
-
-                            Output.WriteLineStandard($"extracted to '{dirToExtract.FullName}'");
-
-                            var dirTarget = new DirectoryInfo(targetDirArgument.Value.TrimPath());
-
-                            if (!dirTarget.Exists)
-                            {
-                                dirTarget.Create();
-                                Output.WriteLineStandard($"install starting");
-                            }
-                            else
-                                Output.WriteLineStandard($"update starting");
-
-                            if (process != null)
-                                process.WaitForExit(1000 * 120);
-
-                            result = dirToExtract.MoveTo(dirTarget);
-
-                            if (result == 0)
-                                dirToExtract.Delete();
-
-                        }
-                        else
-                        {
-                            Output.WriteLineError($"Failed to extract {file.FullName}");
-                            return 2;
-                        }
-
-                        file.Delete();
-
-                    }
-                    else
-                    {
-                        Output.WriteLineError($"Failed to download {artefact.UrlDownload}");
-                        return 1;
-                    }
-
-                    if (result == 0)
-                        Output.WriteLineStandard($"succefully updated");
-                    else
-                        Output.WriteLineError($"file haven't replaced. please ensure all process are close and retry");
-
-                    return 0;
+                    return _result;
 
                 });
             });
